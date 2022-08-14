b0ac2ca4ab2bbe4d0812cbfa5a075a4a0714ae9e
Mohamed Mohamed <mohamedmohamed@Mohameds-MacBook-Pro.local>
2022-08-13 21:28:14 -0500

    we working

--- script.js
+++ script.js
@@ -51,34 +51,35 @@ function displayCurrentWeather(city, date, temp, humidity, windSpeed, weatherIco
     $("#current-weather-conditions").append(cardDiv);
 }
 
-function displayFiveDayForecast(date, temp, windSpeed, humidity, iconUrl) {
-    const cardEl = $("<div class='card'>").addClass("pl-1 bg-primary text-light");
-    const cardBlockDiv = $("<div>").attr("class", "card-block");
-    const cardTitleDiv = $("<div>").attr("class", "card-block");
-    const cardTextDiv = $("<div>").attr("class", "card-text");
-    const imgEl = $("<img>").attr("src", iconUrl);
-    const cardTitleHeader = $("<h6>").text(date).addClass("pt-2");
-    const tempEl = $("<p>").text(`Temp: ${temp} ºF"`).css("font-size", "0.60rem");
-    const windSpeedEl = $("<p>").text(`Wind: ${windSpeed} MPH"`).css("font-size", "0.60rem");
-    const humidityEl = $("<p>").text(`Humidity: ${humidity} %`).css("font-size", "0.60rem");
+function displayFiveDayForecast(forecast) {
+    for (let i=3; i < forecast.length; i+=8) {
+        const currentDay = forecast[i]
+        const date = moment.unix(currentDay.dt).format("l");
+        const temp = currentDay.main.temp
+        const windSpeed = currentDay.wind.speed
+        const humidity = currentDay.main.humidity
+        const iconUrl = `https://openweathermap.org/img/w/${currentDay.weather[0].icon}.png`
+        const cardEl = $("<div class='card'>").addClass("pl-1 bg-primary text-light");
+        const cardBlockDiv = $("<div>").attr("class", "card-block");
+        const cardTitleDiv = $("<div>").attr("class", "card-block");
+        const cardTextDiv = $("<div>").attr("class", "card-text");
+        const imgEl = $("<img>").attr("src", iconUrl);
+        const cardTitleHeader = $("<h6>").text(date).addClass("pt-2");
+        const tempEl = $("<p>").text(`Temp: ${temp} ºF"`).css("font-size", "0.60rem");
+        const windSpeedEl = $("<p>").text(`Wind: ${windSpeed} MPH"`).css("font-size", "0.60rem");
+        const humidityEl = $("<p>").text(`Humidity: ${humidity} %`).css("font-size", "0.60rem");
 
-    cardTextDiv.append(imgEl);
-    cardTextDiv.append(tempEl);
-    cardTextDiv.append(windSpeedEl);
-    cardTextDiv.append(humidityEl);
-    cardTitleDiv.append(cardTitleHeader);
-    cardBlockDiv.append(cardTitleDiv);
-    cardBlockDiv.append(cardTextDiv);
-    cardEl.append(cardBlockDiv);
-    $(".card-deck").append(cardEl);
+        cardTextDiv.append(imgEl);
+        cardTextDiv.append(tempEl);
+        cardTextDiv.append(windSpeedEl);
+        cardTextDiv.append(humidityEl);
+        cardTitleDiv.append(cardTitleHeader);
+        cardBlockDiv.append(cardTitleDiv);
+        cardBlockDiv.append(cardTextDiv);
+        cardEl.append(cardBlockDiv);
+        $(".card-deck").append(cardEl);
+    }
 }
-
-function addCardDeckHeader() {
-    deckHeader = $("<h4>").text("5-Day Forecast").attr("id", "card-deck-title");
-    deckHeader.addClass("pt-4 pt-2");
-    $(".card-deck").before(deckHeader);
-}
-
 // To clear html of current searched city data to make way for next searched city data
 function clearDisplayedWeatherInfo() {
     $("#current-weather-conditions").empty();
@@ -95,7 +96,6 @@ function searchCity(cityName) {
 
         // store all of the retrieved data inside of an object called "response"
         .then((response) => {
-            console.log(response)
             searchedCity = response.name.trim();
             currentDate = moment.unix(response.dt).format("l");
             currentTemp = response.main.temp;
@@ -109,34 +109,22 @@ function searchCity(cityName) {
                 method: "GET"
             })
                 .then((response) => {
-
-                    console.log('this is 5 day', response)
+                    $("#five-day-forecast-title").css("display", "block");
                     const fiveDayForecast = response.list;
+                    displayFiveDayForecast(fiveDayForecast)
+                    // for (let i = 3; i < fiveDayForecast.length; i++) {
+                    //     console.log(i)
 
-                    for (let i = 3; i < fiveDayForecast.length; i+8) {
-                        console.log('here')
-                        // const currentDay = fiveDayForecast[i]
-                        // const date = moment.unix(currentDay.dt).format("l");
-                        // const temp = currentDay.main.temp
-                        // const windSpeed = currentDay.wind.speed
-                        // const humidity = currentDay.main.humidity
-                        // const icon = `https://openweathermap.org/img/w/${currentDay.weather[0].icon}.png`
+                    //     const currentDay = fiveDayForecast[i]
+                    //     const date = moment.unix(currentDay.dt).format("l");
+                    //     const temp = currentDay.main.temp
+                    //     const windSpeed = currentDay.wind.speed
+                    //     const humidity = currentDay.main.humidity
+                    //     const icon = `https://openweathermap.org/img/w/${currentDay.weather[0].icon}.png`
 
-                        // displayFiveDayForecast(date, temp, windSpeed, humidity, icon)
-                    }
-                    //    addCardDeckHeader()
-                    //    for (let i=0; i < 5; i++) {
-                    //      iconcode = fiveDayForecast[i].weather[0].icon;
-                    //      iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
-                    //     //  dateValue = moment().tz(country + "/" + city).add(i, 'days').format('l');
-                    //     dateValue = moment.unix(fiveDayForecast[i].dt).format('l');
-                    //      minTempK = fiveDayForecast[i].temp.min;
-                    //      minTempF =  ((minTempK - 273.15) * 1.80 + 32).toFixed(1);
-                    //      maxTempK = fiveDayForecast[i].temp.max;
-                    //      maxTempF =  (((fiveDayForecast[i].temp.max) - 273.15) * 1.80 + 32).toFixed(1);
-                    //      dayhumidity = fiveDayForecast[i].humidity;
-                    //      displayFiveDayForecast()
-                    //    } 
+                    //     return displayFiveDayForecast(date, temp, windSpeed, humidity, icon)
+
+                    // }
                 });
         });
     //  });
